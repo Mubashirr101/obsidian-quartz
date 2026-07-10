d76815ca9dcfc1b4532ce8f507204f5f7ab6261a
Mubashir <zerex68@gmail.com>
2026-07-10 10:11:14 +0530

    Quartz sync: Jul 10, 2026, 10:11 AM

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,9 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./.quartz/plugins"
+
+ExternalPlugin.Explorer({
+  filterFn: (node) => node.displayName.toLowerCase() !== "changelog-data",
+})
 
 const config = await loadQuartzConfig()
 export default config
