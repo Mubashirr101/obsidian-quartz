50ae9c215950ee067870a3bbbc144caf1ab66ad8
Mubashir <zerex68@gmail.com>
2026-07-11 18:42:28 +0530

    Quartz sync: Jul 11, 2026, 6:42 PM

--- quartz.ts
+++ quartz.ts
@@ -1,12 +1,11 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 import * as ExternalPlugin from "./.quartz/plugins"
 
-const hiddenFromExplorer = ["changelog-data", "assets", "tags"]
-
 ExternalPlugin.Explorer({
   filterFn: (node) => {
+    const hidden = ["changelog-data", "assets", "tags"]
     const name = node.displayName?.toLowerCase() ?? ""
-    return !hiddenFromExplorer.includes(name)
+    return !hidden.includes(name)
   },
 })
 
